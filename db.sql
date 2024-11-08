ebd8e60db98b49f294455e9c83c1d973b7db6c40
Eichi2514 <picon1317@gmail.com>
2024-11-08 14:38:58 +0900

    샘플 회원 2명 생성

---
+++ db.sql
@@ -0,0 +1,19 @@
+show databases;
+
+DROP DATABASE IF EXISTS `surl_dev`;
+CREATE DATABASE `surl_dev`;
+USE `surl_dev`;
+
+show tables;
+
+desc article;
+
+TRUNCATE article
+
+SELECT *
+FROM article
+
+         TRUNCATE member
+
+SELECT *
+FROM member
