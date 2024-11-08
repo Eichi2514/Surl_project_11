48623aad902db99672d070ea1f3f8a5e3d6cd85d
Eichi2514 <picon1317@gmail.com>
2024-11-08 11:28:52 +0900

    게시글 셀렉트

--- src/main/java/com/koreait/surl_project_11/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/repository/ArticleRepository.java
@@ -2,7 +2,7 @@ package com.koreait.surl_project_11.domain.article.article.repository;
 
 
 import com.koreait.surl_project_11.domain.article.article.entity.Article;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
