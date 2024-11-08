5fb1cfd49e3fb1c515b2b1de2b75a1ffb9572f9f
Eichi2514 <picon1317@gmail.com>
2024-11-08 13:43:23 +0900

    RsData 도입

--- src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
@@ -41,9 +41,8 @@ public class NotProd {
     public void work1() {
         if (articleService.count() > 0) return;
 
-        //articleRepository.deleteAll();
-        Article article1 = articleService.write("제목 1", "내용1");
-        Article article2 = articleService.write("제목 2", "내용2");
+        Article article1 = articleService.write("제목 1", "내용 1").getData();
+        Article article2 = articleService.write("제목 2", "내용 2").getData();
 
         article2.setTitle("제목 2-2");
 
