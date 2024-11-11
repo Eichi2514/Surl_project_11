41b08cc25b7a920c62533574fd5838bc8cc91848
Eichi2514 <picon1317@gmail.com>
2024-11-11 16:29:51 +0900

    output 추가

--- src/main/java/com/koreait/surl_project_11/global/initData/All.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/All.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
-// !prod == dev or test
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
