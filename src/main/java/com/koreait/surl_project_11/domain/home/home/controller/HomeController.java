fded087f299aecf237e5da47076d12f09d2b56af
Eichi2514 <picon1317@gmail.com>
2024-11-14 16:13:55 +0900

    test1

--- src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String ShowMain() {
-        return "Main6 on " + customSiteName;
+        return "Main1 on " + customSiteName;
     }
 
     @GetMapping("/secretKey")
