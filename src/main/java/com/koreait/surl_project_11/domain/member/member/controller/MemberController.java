4a6b5ff76921ebe11899405e0e94fb0a80fae83a
Eichi2514 <picon1317@gmail.com>
2024-11-15 14:43:22 +0900

    ExceptionHandler를 이용한 예외처리 추가

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
@@ -5,35 +5,42 @@ import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import com.koreait.surl_project_11.standard.util.Ut;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
-
-    private MemberService memberService;
+    private final MemberService memberService;
 
     @GetMapping("/join")
     @ResponseBody
-    public RsData join(String username, String password, String nickname) {
-
-        if(Ut.str.isBlank(username)){
-            // return RsData.of("400-1", "이름을 입력해");
-            throw new GlobalException("400-1", "이름을 입력해");
-        }
-        if(Ut.str.isBlank(password)){
-            // return RsData.of("400-1", "비밀번호를 입력해");
-            throw new GlobalException("400-1", "비밀번호를 입력해");
-        }
-        if(Ut.str.isBlank(nickname)){
-            // return RsData.of("400-1", "닉네임을 입력해");
-            throw new GlobalException("400-1", "닉네임을 입력해");
+    public RsData<Member> join(
+            String username, String password, String nickname
+    ) {
+        if (Ut.str.isBlank(username)) {
+            throw new GlobalException("400-1", "username을 입력해");
         }
 
-        RsData<Member> joinRs = memberService.join(username, password, nickname);
+        if (Ut.str.isBlank(password)) {
+            throw new GlobalException("400-2", "password을 입력해");
+        }
 
-        return joinRs;
+        if (Ut.str.isBlank(nickname)) {
+            throw new GlobalException("400-3", "nickname을 입력해");
+        }
+
+        return memberService.join(username, password, nickname);
+
     }
 
+    @GetMapping("/testThrowIllegalArgumentException")
+    @ResponseBody
+    public RsData<Member> testThrowIllegalArgumentException() {
+        throw new IllegalArgumentException("IllegalArgumentException");
+    }
 }
