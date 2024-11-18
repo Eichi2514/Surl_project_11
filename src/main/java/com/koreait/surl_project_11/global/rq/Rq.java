92085837c311f6de67b52ab452a998c4430ebf36
Eichi2514 <picon1317@gmail.com>
2024-11-18 15:31:54 +0900

    Aspect, RsData 상태코드를 응답코드로 사용

--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -3,6 +3,7 @@ package com.koreait.surl_project_11.global.rq;
 import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -12,7 +13,7 @@ import org.springframework.web.context.annotation.RequestScope;
 @RequiredArgsConstructor
 public class Rq {
     private final HttpServletRequest req;
-    // private final HttpServletResponse resp;
+    private final HttpServletResponse resp;
     private final MemberService memberService;
 
     public Member getMember() {
@@ -21,5 +22,9 @@ public class Rq {
 
     public String getCurrentUrlPath() {
         return req.getRequestURI();
+    }
+
+    public void setStatusCode(int statusCode) {
+        resp.setStatus(statusCode);
     }
 }
