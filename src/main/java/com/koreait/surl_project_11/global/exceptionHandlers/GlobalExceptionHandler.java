9ba0e45f97c850d4e3dac264bdd77c97d94cd427
Eichi2514 <picon1317@gmail.com>
2024-11-18 13:59:21 +0900

    예외 발생시 상태코드 예외에 맞게 바뀌게 수정

--- src/main/java/com/koreait/surl_project_11/global/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/koreait/surl_project_11/global/exceptionHandlers/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
 package com.koreait.surl_project_11.global.exceptionHandlers;
 
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
+import com.koreait.surl_project_11.global.rsData.RsData;
+import com.koreait.surl_project_11.standard.dto.Empty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @ControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public String handleException(Exception ex) {
-        log.debug("handleException 1");
-        return "<a href=\"../all\" style=\"color: black; text-underline-offset: 5px;\">" + ex.getMessage() + "</a>";
-    }
     @ExceptionHandler(GlobalException.class)
+    //@ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public String handleException(GlobalException ex) {
-        log.debug("handleException 2");
-        return "<a href=\"../all\" style=\"color: black; text-underline-offset: 5px;\">" + ex.getMessage() + "</a>";
+    public ResponseEntity<String> handleException(GlobalException ex) {
+        RsData<Empty> rsData = ex.getRsData();
+
+        rsData.getStatusCode();
+
+        return ResponseEntity.status(rsData.getStatusCode()).body(rsData.getMsg());
     }
 }
