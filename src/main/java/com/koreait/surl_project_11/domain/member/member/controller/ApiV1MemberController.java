5807ffd4b0115c1331ef9febd2724ccf86c5379a
Eichi2514 <picon1317@gmail.com>
2024-11-18 15:15:34 +0900

    액션의 응답본문 클래스 추가

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
@@ -37,17 +37,21 @@ public class ApiV1MemberController {
         private String nickname;
     }
 
-    // POST /api/v1/members
-    @PostMapping("")
-    public RsData<Member> join(
-            @RequestBody @Valid MemberJoinReqBody requestBody
-    ) {
-
-        int a = 40;
-        int b = 0;
-        int c = a / b;
-
-        return memberService.join(requestBody.username, requestBody.password, requestBody.nickname);
+    @AllArgsConstructor
+    @Getter
+    public static class MemberJoinRespBody {
+        Member item;
     }
 
+    // POST /api/v1/members
+    @PostMapping("")
+    public RsData<MemberJoinRespBody> join(
+            @RequestBody @Valid MemberJoinReqBody requestBody
+    ) {
+        RsData<Member> joinRs = memberService.join(requestBody.username, requestBody.password, requestBody.nickname);
+
+        return joinRs.newDataOf(
+                new MemberJoinRespBody(joinRs.getData())
+        );
+    }
 }
