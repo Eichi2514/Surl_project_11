f3937866689540da4286fe9fd693ec820a84c5c3
Eichi2514 <picon1317@gmail.com>
2024-12-16 13:37:10 +0900

    백엔드 통신용 타입스크립트 자동생성

--- src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -3,7 +3,10 @@ package com.koreait.surl_project_11.standard.util;
 import com.koreait.surl_project_11.global.app.AppConfig;
 import lombok.SneakyThrows;
 
+import java.util.concurrent.TimeUnit;
+
 public class Ut {
+
     public static class str {
         public static boolean isBlank(String str) {
             return str == null || str.isBlank();
@@ -19,6 +22,23 @@ public class Ut {
         @SneakyThrows
         public static String toString(Object obj) {
             return AppConfig.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        }
+    }
+
+    public static class cmd {
+        public static void runAsync(String cmd) {
+            new Thread(() -> {
+                run(cmd);
+            }).start();
+        }
+        public static void run(String cmd) {
+            try {
+                ProcessBuilder processBuilder = new ProcessBuilder(cmd.split(" "));
+                Process process = processBuilder.start();
+                process.waitFor(1, TimeUnit.MINUTES);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
